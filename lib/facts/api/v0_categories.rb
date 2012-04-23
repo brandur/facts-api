b2ce5b4f1ee99a72e0421f5005afd6170f4e0b9e
Brandur <brandur@mutelight.org>
2012-04-22 23:14:24 -0700

    Recursive category update

--- lib/facts/api/v0_categories.rb
+++ lib/facts/api/v0_categories.rb
@@ -12,6 +12,47 @@ module Facts
         def serialize(obj)
           @@serializer.serialize(obj)
         end
+
+        def update_categories(category, category_attrs_arr)
+          categories_not_updated = Hash[*category.categories.map{ |c| [c.id, c]}.flatten]
+          category_attrs_arr.each do |category_attrs|
+            new_category = category.categories.find_by_slug(category_attrs["slug"])
+            if new_category
+              update_category(new_category, category_attrs)
+            else
+              category.categories.create!(category_attrs)
+            end
+          end
+
+          # delete any categories that aren't supposed to be here anymore
+          categories_not_updated.each{ |id, category| category.destroy }
+        end
+
+        def update_category(category, category_attrs)
+          category_attrs_arr = category_attrs.delete("categories")
+          update_categories(category, category_attrs_arr) if category_attrs_arr
+
+          fact_attrs_arr = category_attrs.delete("facts")
+          update_facts(category, fact_attrs_arr) if fact_attrs_arr
+
+          category.update_attributes(category_attrs)
+        end
+
+        def update_facts(category, fact_attrs_arr)
+          facts_not_updated = Hash[*category.facts.map{ |f| [f.id, f]}.flatten]
+          fact_attrs_arr.each do |fact_attrs|
+            fact = category.facts.find_by_content(fact_attrs["content"])
+            if fact
+              fact.update_attributes(fact_attrs)
+              facts_note_updated.delete(fact.id)
+            else
+              category.facts.create!(fact_attrs)
+            end
+          end
+
+          # delete any facts that aren't supposed to be here anymore
+          facts_not_updated.each{ |id, fact| fact.destroy }
+        end
       end
 
       resource :categories do
@@ -39,7 +80,9 @@ module Facts
           require_params!(:category)
           category = Models::Category.find_by_path!(params[:path])
           attrs = params[:category].parse_json
-          category.update_attributes(attrs)
+          Models::Category.transaction do
+            update_category(category, attrs)
+          end
           serialize(category)
         end
 
--- test/api/v0_categories_test.rb
+++ test/api/v0_categories_test.rb
@@ -6,7 +6,7 @@ module Facts
       include Rack::Test::Methods
 
       let(:category1) { Models::Category.new(id: 1, name: "World", slug: "world") }
-      let(:category2) { Models::Category.new(id: 2, name: "Canada", slug: "world/canada") }
+      let(:category2) { Models::Category.new(id: 2, name: "Canada", slug: "canada", category_id: category1.id) }
 
       def app
         Facts::ApiAggregate
@@ -65,6 +65,72 @@ module Facts
         delete "/v0/categories/world/canada"
         last_response.status.must_equal 200
         last_response.body.must_equal ""
+      end
+
+      describe "recursive update" do
+        before do
+          category1.save!
+          category1.reload
+          category2.save!
+        end
+
+        it "adds facts to a category" do
+          attrs = { category_id: 1, name: "Canada", slug: "canada", facts: [
+            { content: "The world is big." },
+          ]}
+          put "/v0/categories/world/canada", :category => attrs.to_json
+          last_response.status.must_equal 200
+          category2.reload
+          category2.facts.count.must_equal 1
+          category2.facts.first.content.must_equal "The world is big."
+        end
+
+        it "removes facts from a category" do
+          category2.facts.create! content: "The world is big."
+          attrs = { category_id: 1, name: "Canada", slug: "canada", facts: [] }
+          put "/v0/categories/world/canada", :category => attrs.to_json
+          last_response.status.must_equal 200
+          category2.reload
+          category2.facts.count.must_equal 0
+        end
+
+        it "doesn't remove facts from a category on a nil" do
+          category2.facts.create! content: "The world is big."
+          attrs = { category_id: 1, name: "Canada", slug: "canada", facts: nil }
+          put "/v0/categories/world/canada", :category => attrs.to_json
+          last_response.status.must_equal 200
+          category2.reload
+          category2.facts.count.must_equal 1
+        end
+
+        it "adds a subcategory" do
+          attrs = { category_id: 1, name: "Canada", slug: "canada", categories: [
+            { name: "Alberta", slug: "alberta" },
+          ]}
+          put "/v0/categories/world/canada", :category => attrs.to_json
+          last_response.status.must_equal 200
+          category2.reload
+          category2.categories.count.must_equal 1
+          category2.categories.first.slug.must_equal "alberta"
+        end
+
+        it "removes a subcategory" do
+          category2.categories.create! name: "Alberta", slug: "alberta"
+          attrs = { category_id: 1, name: "Canada", slug: "canada", categories: [] }
+          put "/v0/categories/world/canada", :category => attrs.to_json
+          last_response.status.must_equal 200
+          category2.reload
+          category2.categories.count.must_equal 0
+        end
+
+        it "doesn't remove subcategories on a nil" do
+          category2.categories.create! name: "Alberta", slug: "alberta"
+          attrs = { category_id: 1, name: "Canada", slug: "canada", categories: nil }
+          put "/v0/categories/world/canada", :category => attrs.to_json
+          last_response.status.must_equal 200
+          category2.reload
+          category2.categories.count.must_equal 1
+        end
       end
 
       private
